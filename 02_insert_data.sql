e3a9df6c7f782f27562227d1aa3e2ad7374f6481
John Paulo Limpiahoy <limpiahoyjohnpaulo_bsit@plmun.edu.ph>
2025-03-16 21:20:45 +0800

    Lab002

---
+++ 02_insert_data.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO tblStudents (student_name, student_email, student_phone)
+VALUES
+    ('John Doe', 'johndoe@gmail.com', '123-456-7890'),
+    ('Jane Doe', 'janedoe@gmail.com', '123-456-7890');
